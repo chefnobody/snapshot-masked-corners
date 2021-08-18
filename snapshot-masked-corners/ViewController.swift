7b8f9a07c6a996412fa67905eb4e35638ad2f2d9
Aaron Connolly <aaron.connolly@carvana.com>
2021-08-18 11:15:32 -0700

    Initial Commit

---
+++ snapshot-masked-corners/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snapshot-masked-corners
+//
+//  Created by Aaron Connolly on 8/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
